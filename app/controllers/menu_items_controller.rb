7157e3b9f08e3ffeb29d9b1b1437b850766fb747
Ajdin Imsirovic <ajdika@live.com>
2022-01-17 17:15:14 +0100

    Define CRUD actions on menu items in the controller

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -1,2 +1,37 @@
 class MenuItemsController < ApplicationController
+    def index
+        @menuItems = MenuItem.all
+        render json: @menuItems
+    end
+
+    def show
+        @menuItem = MenuItem.find(params[:id])
+        render json: @menuItem
+    end
+
+    def create
+        @menuItem = MenuItem.create(
+            menu_name: params[:menu_name],
+            restaurant_name: params[:restaurant_name],
+            menu_description: params[:menu_description]
+        )
+        render json: @menuItem
+    end
+
+    def update
+        @menuItem = MenuItem.find(params[:id])
+        @menuItem.update(
+            menu_name: params[:menu_name],
+            restaurant_name: params[:restaurant_name],
+            menu_description: params[:menu_description]
+        )
+        render json: @menuItem
+    end
+
+    def destroy
+        @menuItems = MenuItem.all
+        @menuItem = MenuItem.find(params[:id])
+        @menuItem.destroy
+        render json: @menuItems
+    end
 end
