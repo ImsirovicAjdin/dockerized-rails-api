0b4277562f7b66d22cd603f81c6f91b90524b3a1
Ajdin Imsirovic <ajdika@live.com>
2022-01-17 17:46:17 +0100

    Seed the db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# in db>seed.rb
+
+menu1 = MenuItem.create(menu_name: "Burek", restaurant_name: "Pie shop", menu_description:"Excellent burek with yogurt")
+menu2 = MenuItem.create(menu_name: "Cevapi", restaurant_name: "Zeljo", menu_description:"The best cevapi from Sarajevo")
+menu3 = MenuItem.create(menu_name: "Boza", restaurant_name: "Evropa", menu_description:"Perfect summer drink")
